8128c92fb0afddce3323e9e88590b828f1260f72
endiesworld <endiesworld@gmail.com>
2019-11-26 02:28:17 +0100

    updated mobile navbar and display header

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import './scss/style.scss';
 import './css/izicss.css';
 import { header, showElement } from './js/mobileView'
 
-const reLoadPage = () => {
-    header();
+
+const screeWidth = () =>{
+    return window.innerWidth ;
+  }
+  const reLoadPage = () => {
+    header(window.innerWidth);
 }
 const eventListenerRegister = () => {
     let element = document.querySelector(".navbarButton");
     element.addEventListener("click", showElement);
     window.addEventListener("resize", reLoadPage);
 }
-header(window.innerWidth) ;
+reLoadPage() ;
 eventListenerRegister() ;
