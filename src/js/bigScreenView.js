2c7f9aaf4bf4e5cc9b99050b045a2b9bb68f140d
endiesworld <endiesworld@gmail.com>
2019-12-11 17:03:15 +0100

    perfecting navbar responsiveness for both mobile and widescreen

--- src/js/bigScreenView.js
+++ src/js/bigScreenView.js
@@ -1,7 +1,7 @@
 export const addBootstrapNavElement = () => {
-    let element = document.querySelector(".navbarResponsive") ;
-    if (!element.classList.contains("collapse")) {
-        element.className += " collapse navbar-collapse"; // To ensure that the normal attains normal bootstrap navbar behavior
-      }
-    element.style.display = "block" ;
+  let element = document.querySelector("#navbarSupportedContent");
+  if (!element.classList.contains("collapse")) {
+    element.className += " collapse navbar-collapse"; // To ensure that the normal attains normal bootstrap navbar behavior
+  }
+  //element.style.display = "block";
 }
--- src/js/mobileView.js
+++ src/js/mobileView.js
@@ -1,23 +1,25 @@
-import iPhoneLogo from "../images/mobileApple2.png" ;
-import androidLogo from "../images/mobileGoogle2.png" ;
 
-let getQuerySelectorElement =(element) => {
-    return document.querySelector(element) ;
-} 
+
+import iPhoneLogo from "../images/mobileApple2.png";
+import androidLogo from "../images/mobileGoogle2.png";
+
+let getQuerySelectorElement = (element) => {
+    return document.querySelector(element);
+}
 
 export const showElement = () => {
-    let element = getQuerySelectorElement(".navbarResponsive");
-    element.style.width = "50%" ;
-   // element.style.display = "block" ;
+    let element = getQuerySelectorElement("#navbarSupportedContent");
+    element.classList.remove("collapse", "navbar-collapse");
+    //element.style.width = "50%";
+    // element.style.display = "block" ;
     //element.classList.remove("navbarResponsive" );
     //element.className+= " sidepanel" ;
 }
 
 export const hideNavBarElement = () => {
-    let element = getQuerySelectorElement(".navbarResponsive");
-    let navbarImages = getQuerySelectorElement(".appStoreImages") ;
-    navbarImages.style.display = "none" ;
-    element.style.width = "0";
-    element.classList.remove("collapse" , "navbar-collapse" );
+    let element = getQuerySelectorElement("#navbarSupportedContent");
+    let navbarImages = getQuerySelectorElement(".appStoreImages");
+    navbarImages.style.display = "none";
+    //element.style.width = "0";
+    element.classList.remove("collapse", "navbar-collapse");
 }
-
